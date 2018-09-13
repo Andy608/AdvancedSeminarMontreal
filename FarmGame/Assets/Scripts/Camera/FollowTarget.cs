30cc50c75d6c77de8f6523921c2a0f704209d59a
Andy608 <AndrewR608@gmail.com>
2018-09-13 17:21:07 -0400

    Started work on basic player movement.

---
+++ FarmGame/Assets/Scripts/Camera/FollowTarget.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    //The offset from where the camera is to the target.
+    public Vector3 offset = new Vector3();
+
+    //How far away can the target before the camera starts to move towards it?
+    //public float followDistance = 3.0f;
+    //public float distanceTilStop = 1.0f;
+
+    //bool followTarget = false;
+
+    private void Start()
+    {
+        //Square it so we can use magnitude squared later on in the update method.
+        //followDistance *= followDistance;
+    }
+
+    private void Update()
+    {
+        Vector2 targetPosition = targetObject.transform.position;
+        Vector2 cameraPosition = transform.position;
+        float distance = (targetPosition - cameraPosition).sqrMagnitude;
+
+        //if (distance >= followDistance)
+        //{
+        //    followTarget = true;
+        //}
+
+        //if (distance <= distanceTilStop)
+        //{
+        //    followTarget = false;
+        //}
+
+        //if (followTarget)
+        //{
+            transform.position = Vector3.Lerp(transform.position, targetObject.transform.position + offset, 10.0f * Time.deltaTime);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetObject.transform.rotation, 10.0f * Time.deltaTime);
+        //}
+    }
+}
---
+++ FarmGame/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private Vector2 velocity = new Vector2();
+    private Vector3 lookDirection = new Vector2();
+
+    private Vector3 mouseWorldPosition = new Vector3();
+
+    public float moveSpeed = 10.0f;
+
+    public float minimumMouseDistance = 0f;
+
+    private void Update()
+    {
+        UpdateLookDirection();
+
+        if (!IsMouseTooClose())
+        {
+            RotatePlayer();
+        }
+
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        velocity.x = Input.GetAxis(HORIZONTAL) * Time.deltaTime * moveSpeed;
+        velocity.y = Input.GetAxis(VERTICAL) * Time.deltaTime * moveSpeed;
+
+        transform.Translate(velocity);
+    }
+
+    private void RotatePlayer()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90.0f));
+    }
+
+    private void UpdateLookDirection()
+    {
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0;
+
+        lookDirection = mouseWorldPosition - transform.position;
+    }
+
+    private bool IsMouseTooClose()
+    {
+        return lookDirection.sqrMagnitude <= minimumMouseDistance;
+    }
+}
