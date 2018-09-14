985b8282c6d0d5ea7d5f233ce44b28861254736c
pigshark27 <idk>
2018-09-13 20:48:07 -0400

    Working on making player movement better.

---
+++ FarmGame/Assets/Scripts/Camera/FollowTargetInViewport.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTargetInViewport : MonoBehaviour 
+{
+	public GameObject targetObject;
+	public float followSpeed = 10.0f;
+	public float edgePadding = 1.0f;
+	
+	//The offset from where the camera is to the target.
+    public Vector3 offset = new Vector3(0, 0, -10);
+
+	private void Update()
+	{
+		if (IsTargetByEdge())
+		{
+			MoveCamera();
+		}
+	}
+
+	private bool IsTargetByEdge()
+	{
+		float verticalHeight = Camera.main.orthographicSize;
+		float horizontalHeight = verticalHeight * Screen.width / Screen.height;
+
+		Vector3 difference = targetObject.transform.position - transform.position;
+
+		if (difference.x < -horizontalHeight + edgePadding || difference.y < -verticalHeight + edgePadding || 
+			difference.x > horizontalHeight - edgePadding || difference.y > verticalHeight - edgePadding)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+
+	private void MoveCamera()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, targetObject.transform.position + offset, followSpeed * Time.deltaTime);
+	}
+}
